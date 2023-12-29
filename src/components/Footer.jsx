a4fbaa458686913fff8d8ef6c01b4021d539ccd5
Jeyeon Kim <wpdus2694@naver.com>
2023-12-30 00:12:03 +0900

    Responsive Patch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 
 function Footer(){
     return <>
-        <div className="w-100 border-top border-secondary p-4 d-flex justify-content-around" style={{backgroundColor:"#282c34", fontSize:"0.8em", position: "fixed", bottom: "0"}}>
+        <div className="w-100 border-top border-secondary p-4 d-flex justify-content-around res-height" style={{backgroundColor:"#282c34", fontSize:"0.8em"}}>
             <Container className="d-flex flex-column text-start text-secondary">
                 <span>Copyright 2023. MOS. all rights reserved.</span>
                 <span>Contact : {process.env.REACT_APP_CONTACT_US}</span>
