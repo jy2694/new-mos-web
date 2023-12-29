947b50fc72bc5f7fce4a1f47de87ca386de8ebf3
Jeyeon Kim <wpdus2694@naver.com>
2023-12-29 23:01:29 +0900

    Footer Edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
         <div className="w-100 border-top border-secondary p-4 d-flex justify-content-around" style={{backgroundColor:"#282c34", fontSize:"0.8em"}}>
             <Container className="d-flex flex-column text-start text-secondary">
                 <span>Copyright 2023. MOS. all rights reserved.</span>
-                <span>Contant : {process.env.REACT_APP_CONTACT_US}</span>
+                <span>Contact : {process.env.REACT_APP_CONTACT_US}</span>
             </Container>
             <Container className="d-flex flex-column text-end text-secondary">
                 <span>회장 : {process.env.REACT_APP_OFFICER_NAME}</span>
